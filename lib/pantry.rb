7dc07fc05461fd268b5cc43da49e81e9cf73ebb0
Chris Powell <chris.ian.powell@gmail.com>
2018-06-05 09:34:54 -0600

    completed pantry class

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,23 @@
 class Pantry
+  attr_reader :stock
 
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item)
+    if @stock[item] != nil
+      @stock[item]
+    else
+      0
+    end
+  end
+
+  def restock(item, unit)
+    if @stock[item] != nil
+      @stock[item] += unit
+    else
+      @stock[item] = unit
+    end
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,31 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+  end
 
+  def test_it_has_attributes
+    assert_equal ({}), @pantry.stock
+  end
+
+  def test_stock_check
+    test_stock = @pantry.stock_check('Cheese')
+
+    assert_equal 0, test_stock
+  end
+
+  def test_restock
+    stock = 'Cheese'
+    unit_1 = 10
+    unit_2 = 20
+
+    @pantry.restock(stock, unit_1)
+
+    assert_equal 10, @pantry.stock_check('Cheese')
+
+    @pantry.restock(stock, unit_2)
+
+    assert_equal 30, @pantry.stock_check('Cheese')
+  end
 end
